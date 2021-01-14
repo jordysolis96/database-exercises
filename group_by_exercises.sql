2fb70577459e2efdaec970baff0f19886090d353
jordy muniz <jordy.muniz96@gmail.com>
2021-01-14 10:19:57 -0600

    Groub by exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+SELECT COUNT(first_name), gender
+FROM employees
+WHERE (first_name = 'Irena'OR first_name = 'Vidya' OR first_name = 'Maya')
+GROUP BY gender;
